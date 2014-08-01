a5103759073c62a596fc1dd1ff6a7135b5ef7934
FriendlyWP <michelle@friendlywebconsulting.com>
2014-08-01 16:15:12 -0500

    Footer stuff

--- footer.php
+++ footer.php
@@ -28,7 +28,7 @@
 					<?php if (function_exists('get_field') && get_field('street_address', 'option')) {
 						$email = get_field('contact_email', 'option');
 						?>
-						<p itemscope itemtype="http://schema.org/Organization" class="organization-address">
+						<div itemscope itemtype="http://schema.org/Organization" class="organization-address">
 							<span itemprop="name" class="hide"><?php the_field('copyrighted', 'option' ); ?></span>
 							<div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
 						      <span itemprop="streetAddress"><?php the_field('street_address', 'option'); ?></span>,
@@ -36,10 +36,11 @@
 						      <span itemprop="addressRegion"><?php the_field('state', 'option'); ?></span>
 						      <span itemprop="postal-code"><?php the_field('zip', 'option'); ?></span>
 						   </div>
-						   Phone: <span itemprop="tel"><?php the_field('phone', 'option'); ?></span>
-						   <a href="mailto:<?php echo antispambot($email); ?>" target="_blank" itemprop="email">Email Us</a>
-						   <a href="<?php the_field('contact_page_link', 'option'); ?>">Contact Us</a>
-						</p>
+						   <div class="contact">
+							   <span class="item">Phone: <span itemprop="telephone"><?php the_field('phone', 'option'); ?></span></span>
+							   <span class="item"><a href="mailto:<?php echo antispambot($email); ?>" target="_blank" itemprop="email">Email Us</a></span>
+							   <span class="item"><a href="<?php the_field('contact_page_link', 'option'); ?>">Contact Us</a></span>
+						</div>
 						<?php
 					} else { ?> 
 
